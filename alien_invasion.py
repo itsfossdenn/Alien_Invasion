730e5234cef0ffa3fe0e01c6dfb68f80fff1fec4
datadenn <datadenn@gmail.com>
2021-09-06 14:47:51 +0200

    added limit movement when border reached

--- alien_invasion.py
+++ alien_invasion.py
@@ -32,9 +32,10 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+            # detect keypresses & act accordingly
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    # obviously move the shop to the right
                     self.ship.moving_right = True
                 if event.key == pygame.K_LEFT:
                     self.ship.moving_left = True
@@ -43,8 +44,6 @@ class AlienInvasion:
                     self.ship.moving_left = False
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = False
-
-
 
     def _check_screen(self):
         self.screen.fill(self.settings.bg_color)
--- ship.py
+++ ship.py
@@ -5,6 +5,7 @@ class Ship:
     def __init__(self, ai_game):
         # initialize the ship & starting position
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # load the ship image and get its rect
@@ -13,6 +14,9 @@ class Ship:
 
         # start each new ship at bottom of the screen
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # store a decimal value for the ship's horizontal position
+        self.x = float(self.rect.x)
 
         # movement flag
         self.moving_right = False
@@ -24,7 +28,7 @@ class Ship:
 
     def update(self):
         # update the ships position
-        if self.moving_right:
-            self.rect.x += 1
-        elif self.moving_left:
-            self.rect.x -= 1
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.rect.x += self.settings.ship_speed
+        elif self.moving_left and self.rect.left > 0:
+            self.rect.x -= self.settings.ship_speed

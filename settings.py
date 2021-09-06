e97cc0ca41bd8d3658c5a474d0b44ea33ba3407b
datadenn <datadenn@gmail.com>
2021-09-06 15:34:44 +0200

    final before switching to PyCharm for IDE

--- settings.py
+++ settings.py
@@ -6,3 +6,8 @@ class Settings:
         self.bg_color = (230, 230, 230)
         # ship settings
         self.ship_speed = 2
+        # laser bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
